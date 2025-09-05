b43313114fc5489f505cbae0fdcd1e50c85e1307
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-09-06 01:20:15 +0900

    task dev

---
+++ backend/cmd/root.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+var rootFlags struct {
+	verbose bool
+	port    int
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use: "vite-echo-monorepo server",
+	Run: func(cmd *cobra.Command, args []string) {
+		e := echo.New()
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+		e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", rootFlags.port)))
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&rootFlags.verbose, "verbose", "v", false, "Enable verbose output")
+	rootCmd.Flags().IntVarP(&rootFlags.port, "port", "p", 8080, "Port to run the server on")
+}
---
+++ backend/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/yz4230/vite-echo-monorepo/cmd"
+
+func main() {
+	cmd.Execute()
+}
